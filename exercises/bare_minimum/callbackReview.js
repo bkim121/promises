9b7f878ebdf69dda079f33bc89b63aa3d9764b1a
bkim121 <bkim121@gmail.com>
2017-09-16 10:54:29 -0700

    first 3 tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,8 +6,15 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, ...args) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      args[args.length - 1](err);
+    } else {
+      args[args.length - 1](err, data.toString().split('\n')[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
